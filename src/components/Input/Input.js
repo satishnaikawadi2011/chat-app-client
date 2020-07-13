127a7cba9fbd71b3d2b37a0617e473f8e4772024
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-07-13 22:19:46 +0530

    completed project

---
+++ src/components/Input/Input.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './Input.css';
+import Send from '../../Icons/send.svg';
+
+const Input = ({ setMessage, sendMessage, message }) => (
+	<form className="form">
+		<input
+			className="input"
+			type="text"
+			placeholder="Type a message..."
+			value={message}
+			onChange={({ target: { value } }) => setMessage(value)}
+			onKeyPress={(event) =>
+
+					event.key === 'Enter' ? sendMessage(event) :
+					null}
+		/>
+		<button className="sendButton" onClick={(e) => sendMessage(e)}>
+			<img src={Send} alt="send" className="sendButtonIcon" />
+		</button>
+	</form>
+);
+
+export default Input;
